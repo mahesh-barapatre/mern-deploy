11a6342c773b7b3ae3a76614d05baaee837e7a1d
Mahesh Barapatre <97560633+mahesh-barapatre@users.noreply.github.com>
2023-12-28 00:47:13 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,13 +13,18 @@ app.use(cors({
 }
   ));
 const PORT = process.env.PORT || 5000;
-let connected = "not known";
-// Connect to MongoDB (make sure MongoDB is running)
-mongoose.connect(`mongodb+srv://maheshbarapatre14:maheshAtlas2023@cluster0.tkhncfb.mongodb.net/?retryWrites=true&w=majority`, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(()=>connected="connected").catch(()=>connected="not connected")
-  ;
+const connected = "not known";
+
+
+mongoose.connect(`mongodb+srv://maheshbarapatre14:maheshAtlas2023@cluster0.tkhncfb.mongodb.net/?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+  connected = "done"
+});
 
 // Define a simple schema and model
 const Item = mongoose.model('Item', { name: String });
@@ -39,7 +44,7 @@ app.get('/api/items', async (req, res) => {
 });
 app.get('/', async (req, res) => {
   
-  res.status(200).send("server working", connected);
+  res.status(200).send(`server working ${connected}`);
 });
 
 // API endpoint to add an item to the database
